c922fc9d8dfa67549052fc79de8079f626c884d6
Alhladkiy <Alhladkiy@gmail.com>
2022-06-01 23:01:48 +0300

    feat: add fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,11 @@
+import fs from 'fs'
+
 export const create = async () => {
-    // Write your code here 
+
+    fs.writeFile('src/fs/files/fresh.txt', 'I am fresh and young', (err) => {
+        if (err) throw err;
+        console.log('FS operation failed');
+      });
 };
+
+create();
