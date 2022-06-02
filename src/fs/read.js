79e4c3983ad1a215c0c8ff10765a8a22c37d5268
Alhladkiy <Alhladkiy@gmail.com>
2022-06-02 11:24:57 +0300

    feat: add fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,14 @@
+import fs from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+    const path = 'src/fs/files/fileToRead.txt';
+    fs.readFile(path, 'utf8', (err, fileContent) => {
+        if (err) {
+            console.log('FS operation failed');
+        } else {
+            console.log(fileContent);
+        }
+    });
 };
+
+read();
