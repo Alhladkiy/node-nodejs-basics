eb3e55b4b3808dc22d6e68e9df56c5fa8d05078c
Alhladkiy <Alhladkiy@gmail.com>
2022-06-03 00:26:46 +0300

    feat: add new solution fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,11 +1,21 @@
-import fs from 'fs'
+import fs from 'fs';
 
 export const create = async () => {
-
-    fs.writeFile('src/fs/files/fresh.txt', 'I am fresh and young', (err) => {
-        if (err) throw err;
-        console.log('FS operation failed');
-      });
+  const folder = 'src/fs/files/fresh.txt';
+  const content = 'I am fresh and young';
+  try {
+    if (fs.existsSync(folder)) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.writeFile(folder, content, (err) => {
+        if (err) {
+          throw new Error('FS operation failed');
+        }
+      })
+    } 
+  } catch(err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 create();
