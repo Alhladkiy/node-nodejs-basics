5792f8115c99ffcdcfc76c63f0f4e6ab3a193c70
Alhladkiy <Alhladkiy@gmail.com>
2022-06-03 18:41:09 +0300

    feat: add solution cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,6 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    console.log(`${args[0].slice(2)} is ${args[1]}, ${args[2].slice(2)} is ${args[3]}` )
 };
+
+parseArgs();
