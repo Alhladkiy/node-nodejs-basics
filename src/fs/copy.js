7f1b8a5157f52141184c440697cea565281495bb
Alhladkiy <Alhladkiy@gmail.com>
2022-06-03 15:37:37 +0300

    feat: add solution fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+
 export const copy = async () => {
-    // Write your code here 
+    const pathOldFolder = 'src/fs/files';
+    const pathNewFolder = 'src/fs/files_copy/files';
+    if (!fs.existsSync(pathOldFolder)) {
+        throw new Error('FS operation failed');
+    }
+    if (fs.existsSync(pathNewFolder)) {
+        throw new Error('FS operation failed');
+    } else {
+        fs.mkdir(pathNewFolder, { recursive: true }, (err) => {
+            if (err) {
+                throw err;
+            }
+            fs.readdir(pathOldFolder, {withFileTypes: true}, (err, files) => {
+                if (err) {
+                    throw err;
+                }
+                for (let file of files) {
+                    if (!file.isDirectory()) {
+                        fs.copyFile(path.join(pathOldFolder, file.name), path.join(pathNewFolder, file.name), (err) => {
+                            if (err) {
+                                throw err;
+                            }
+                        });
+                    }
+                }
+            });
+        })
+    }
 };
+
+copy();
