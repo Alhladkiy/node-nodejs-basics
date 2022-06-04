6c55be907b83a1ad290b3d8d6b007f9d95a95d43
Alhladkiy <Alhladkiy@gmail.com>
2022-06-04 18:06:31 +0300

    feat: add update solution fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -2,15 +2,17 @@ import fs from 'fs';
 
 export const list = async () => {
     const folder = 'src/fs/files';
+    let arr = []
     if (fs.existsSync(folder)) {
         fs.readdir(folder, (err, files) => {
             if (err) {
                 throw new Error('FS operation failed');
             } else {
                 files.forEach((item) => {
-                    console.log(item);
+                    arr.push(item);
                 });
             }
+            console.log(arr);
         });
     } else {
         throw new Error('FS operation failed');
