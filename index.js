6595c2f4c48d938e9c5bc9ae26413da4629677a8
Alhladkiy <Alhladkiy@gmail.com>
2022-06-11 21:12:37 +0300

    feat: add command ls

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+
 import readLine  from 'readline';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import { cwd } from 'process';
+import fs from 'fs'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 
 function statProject() {
     const args = process.argv.slice(2);
     args.forEach((item) => {
         const name = item.slice(11);
         console.log(`Welcome to the File Manager, ${name}!`)
-        console.log(`You are currently in ${cwd()}`);
+        console.log(`You are currently in ${ __dirname}`);
         const rl = readLine.createInterface({
             input: process.stdin,
             output: process.stdout
@@ -15,15 +24,34 @@ function statProject() {
             if (line === '.exit') {
                 console.log(`Thank you for using File Manager, ${name}!`)
                 rl.close(); 
+            } 
+            else if (line === 'ls')  {
+                readFiles();
             }
+
         });
         rl.on('SIGINT', (line) => {
             console.log(`Thank you for using File Manager, ${name}!`)
             rl.close(); 
-        })
+        });
+
     })
 }
 
+
+function readFiles () {
+    fs.readdir(__dirname, (err, files) => {
+        const list = [];
+        if (err) {
+            throw new Error('err');
+        } else {
+            files.forEach((item) => {
+                list.push(item);
+            });
+        }
+        console.log(list);
+    });
+}
 statProject();
 
 
